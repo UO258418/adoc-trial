930ff7920634d781f8d56b6e480127a3d9b2ab1b
Magombo <Uo258418@uniovi.es>
2019-03-14 12:13:03 +0100

    test some ejs things

--- learning-ejs/index.js
+++ learning-ejs/index.js
@@ -1,5 +1,5 @@
-// ejs
-const ejs = require("ejs");
+const ejs = require("ejs"); // ejs
+const path = require("path"); // path
 
 // express
 const express = require("express");
@@ -15,7 +15,15 @@ app.listen(port, () => {
 app.set("view engine", "ejs"); // set view engine to ejs
 app.set("views", __dirname + "/views"); // set folder for templated in /views
 
+// set the folder where the stylesheets and scripts will live
+app.use(express.static(path.join(__dirname + "/public")));
+
+const subjects = [
+    {name: "SDI"},
+    {name: "ASR"}
+]
+
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {subjects: subjects});
 });
 
